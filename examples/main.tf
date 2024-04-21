f242c27af26c00ad0abf6bbafad3f5e1f92e3326
Hemanth <hemu.1357@gmail.com>
2024-04-20 20:03:03 -0400

    Modified the main.tf in examples

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,13 @@
 module "my-eks" {
-  source = "../"
+  source                                      = "../"
+  cluster_name                                = "test_eks_cluster"
+  endpoint_private_access                     = false
+  endpoint_public_access                      = true
+  public_access_cidrs                         = ["0.0.0.0/0"]
+  authentication_mode                         = "CONFIG_MAP"
+  bootstrap_cluster_creator_admin_permissions = true
+
+  tags = {
+    Name = "My_eks_cluster"
+  }
 }
